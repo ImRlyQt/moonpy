016bf15f691ff20f89542bb66a9732aa98d3e605
ImRlyQt <ernestlesniak03@gmail.com>
2025-12-05 02:17:48 +0100

    add custom date option

--- date_argument.py
+++ date_argument.py
@@ -7,14 +7,12 @@ def custom_date():
     args = parser.parse_args()
 
     if args.date:
-        # Użytkownik podał datę
         try:
             date = datetime.datetime.strptime(args.date, "%d-%m-%Y")
         except ValueError:
             print("Zły format daty! Użyj DD-MM-YYYY")
             exit(1)
     else:
-        # Brak daty → używamy daty systemowej
         date = datetime.datetime.now(datetime.UTC)
 
     return date
